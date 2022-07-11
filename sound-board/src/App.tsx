c659a77a2ed9a96f860b3ced445c2a1efe18ef23
Guillaume David <guillaumedaavidpro@gmail.com>
2022-07-11 10:57:37 +0200

    create Home component

--- sound-board/src/App.tsx
+++ sound-board/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css';
 // @ts-ignore
 import Header from './components/Header.tsx'
+// @ts-ignore
+import Home from './components/Home.tsx'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
     </div>
   );
 }
