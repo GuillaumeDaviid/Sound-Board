6f89e4d7eee46b114f1d1d1e1e943c80f9158d56
Guillaume David <guillaumedaavidpro@gmail.com>
2022-07-11 11:39:19 +0200

    add sound function

--- sound-board/src/components/Home.tsx
+++ sound-board/src/components/Home.tsx
@@ -1,7 +1,18 @@
 export default function Home () {
+    let audio = new Audio("/greg.mp3")
+
+      const start = () => {
+        audio.play();
+      };
+    
     return(
         <div>
             <h2>Vos sons : </h2>
+
+            <button onClick={start}>Play</button>
+
+            <audio controls src="/greg.mp3" />
+
         </div>
     )
 }
