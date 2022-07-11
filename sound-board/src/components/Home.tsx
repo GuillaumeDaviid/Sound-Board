89323b590b75dc7f608022c5df6174efff012128
Guillaume David <guillaumedaavidpro@gmail.com>
2022-07-11 14:17:11 +0200

    button pause and reset

--- sound-board/src/components/Home.tsx
+++ sound-board/src/components/Home.tsx
@@ -3,15 +3,27 @@ import '../styles/Home.scss'
 export default function Home () {
     let audio = new Audio("/greg.mp3")
 
+    console.log(audio)
+
       const start = () => {
         audio.play();
       };
+
+      const pause = () => {
+        audio.pause();
+      }
+
+      const load = () => {
+        audio.load();
+      }
     
     return(
         <div className="Home">
             <h2>Vos sons : </h2>
 
             <button className="Home_btn" onClick={start}>Play</button>
+            <button onClick={pause}>Pause</button>
+            <button onClick={load}>reset</button>
 
             <audio controls src="/greg.mp3" />
 
