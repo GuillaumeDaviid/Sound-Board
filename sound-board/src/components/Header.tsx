7dcdffe52feecaaab8e748982e6f93805af99d03
Guillaume David <guillaumedaavidpro@gmail.com>
2022-07-12 10:09:55 +0200

    header component: test is working

--- sound-board/src/components/Header.tsx
+++ sound-board/src/components/Header.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
 
 
 export default function Header () {
     return(
         <div>
             <h1>SOUND BOARD</h1>
+            <Link to="/">Home</Link>
         </div>
     )
 }
--- sound-board/src/test/header.test.tsx
+++ sound-board/src/test/header.test.tsx
@@ -2,17 +2,22 @@ import React from 'react'
 // @ts-ignore
 import Header from '../components/Header.tsx'
 import {  render, screen, fireEvent } from '@testing-library/react';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 describe('Header', () => {
     test('Should render without crashing', async () => {
         render(
+            <Router>
             <Header/>
+            </Router>
         );
     })
 
     test('Should title is SOUND BOARD', async () => {
         render(
+            <Router>
             <Header/>
+            </Router>
         );
 
         const titleValue = screen.getByText('SOUND BOARD');
@@ -21,7 +26,9 @@ describe('Header', () => {
 
     test('Should have "home"', async () => {
         render(
+            <Router>
             <Header/>
+            </Router>
         );
 
         const home = screen.getByText('Home');
@@ -30,11 +37,13 @@ describe('Header', () => {
 
     test('Should url = Home when we click on "home"', async () => {
         render(
+            <Router>
             <Header/>
+            </Router>
         );
 
         const home = screen.getByText('Home');
         fireEvent.click(home);
-        expect(global.window.location.pathname).toEqual('/Home');
+        expect(global.window.location.pathname).toEqual('/');
     })
 })
