c604da671984fd21f08a1629ac5aec4ef3d87bc8
Guillaume David <guillaumedaavidpro@gmail.com>
2022-07-13 10:49:03 +0200

    add new song

--- sound-board/src/data/songs.ts
+++ sound-board/src/data/songs.ts
@@ -3,5 +3,10 @@ export const songs = [
         id: 1,
         name : "il est de retour",
         src: "/greg.mp3"
+    },
+    {
+        id: 2,
+        name : "Alors Peut etre",
+        src: "/alors.mp3"
     }
 ]
