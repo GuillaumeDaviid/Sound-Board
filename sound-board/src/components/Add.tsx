d627afc738b9aeb6888b03aa8c1cb2381f267d58
Guillaume David <guillaumedaavidpro@gmail.com>
2022-07-20 11:22:25 +0200

    add component element

--- sound-board/src/components/Add.tsx
+++ sound-board/src/components/Add.tsx
@@ -4,6 +4,15 @@ export default function Add () {
     return(
         <div>
             <h1>Ajouter un son :</h1>
+
+            <h2>Nom : </h2>
+            <input type="text" data-testid="name"/>
+
+            <h2>Télécharger :</h2>
+            <input type="file" name="sound"
+            accept="audio/mp3, " data-testid="file"></input>
+
+            <button>Ajouter</button>
         </div>
     )
 }
